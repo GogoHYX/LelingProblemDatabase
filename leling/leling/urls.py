09b8bd7b0fdc510399122d4994eaf62feaea3bcf
Yuxiang Hu <yuh365@ucsd.edu>
2020-05-19 14:40:55 +0800

    2020/5/19 14:40

--- leling/leling/urls.py
+++ leling/leling/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('uploader/', include('uploader.urls')),
+    path('uploader/', include('loader.urls')),
 ]
--- leling/uploader/models.py
+++ leling/uploader/models.py
@@ -5,57 +5,37 @@ from django.urls import reverse
 from django.utils.timezone import now
 
 
-class BaseModel(models.Model):
+class Question(models.Model):
     id = models.AutoField(primary_key=True)
     created_time = models.DateTimeField('创建时间', default=now)
     last_mod_time = models.DateTimeField('修改时间', default=now)
-
-    def save(self, *args, **kwargs):
-        is_update_views = isinstance(
-            self,
-            Article) and 'update_fields' in kwargs and kwargs['update_fields'] == ['views']
-        if is_update_views:
-            Article.objects.filter(pk=self.pk).update(views=self.views)
-        else:
-            if 'slug' in self.__dict__:
-                slug = getattr(
-                    self, 'title') if 'title' in self.__dict__ else getattr(
-                    self, 'name')
-                setattr(self, 'slug', slugify(slug))
-            super().save(*args, **kwargs)
-
-    def get_full_url(self):
-        site = get_current_site().domain
-        url = "https://{site}{path}".format(site=site,
-                                            path=self.get_absolute_url())
-        return url
-
-    class Meta:
-        abstract = True
-
-    @abstractmethod
-    def get_absolute_url(self):
-        pass
-
-
-class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    
+    class Meta:
+        ordering = ['id']
+        verbose_name = "题目"
+        verbose_name_plural = verbose_name
 
 
-
-class Tag(BaseModel):
+class Tag(models.Model):
     """文章标签"""
     name = models.CharField('标签名', max_length=30, unique=True)
-    slug = models.SlugField(default='no-slug', max_length=60, blank=True)
 
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('blog:tag_detail', kwargs={'tag_name': self.slug})
-
     class Meta:
         ordering = ['name']
         verbose_name = "标签"
+        verbose_name_plural = verbose_name
+
+
+class Subject(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "学科"
         verbose_name_plural = verbose_name
